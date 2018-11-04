37ce8bcdb1d822a9cda86e99a8e1ccd34605e0bc
Lindsay Hauser <>
2018-11-04 18:21:14 -0500

    update the database config files

--- config/dev.exs
+++ config/dev.exs
@@ -68,8 +68,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :task_tracker, TaskTracker.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "task_tracker",
+  password: "webdevhw6",
   database: "task_tracker_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -11,8 +11,8 @@ config :logger, level: :warn
 
 # Configure your database
 config :task_tracker, TaskTracker.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "task_tracker",
+  password: "webdevhw6",
   database: "task_tracker_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
