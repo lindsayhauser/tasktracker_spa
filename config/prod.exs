56548849f18b5821b20bff0ec791797dbdf347d3
Task Tracker 3 SPA <hw8@vultr.guest>
2018-11-08 01:15:10 +0000

    prod config for https

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :task_tracker, TaskTrackerWeb.Endpoint,
   root: ".",
   version: Application.spec(:phoenix_distillery, :vsn),
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", host: "tasks3.lindsayhauser.com", port: 80],
+  url: [scheme: "https", host: "tasks3.lindsayhauser.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
