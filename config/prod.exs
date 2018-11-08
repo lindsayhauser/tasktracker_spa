61f60de5ebebb806135e4bb406d4cf92fe2b079c
Lindsay Hauser <>
2018-11-07 19:03:07 -0500

    config prod to use https

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,8 @@ config :task_tracker, TaskTrackerWeb.Endpoint,
   server: true,
   root: ".",
   version: Application.spec(:phoenix_distillery, :vsn),
-  https: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "tasks3.lindsayhauser.com", port: 80],
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [scheme: "https", host: "tasks3.lindsayhauser.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
